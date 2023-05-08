05aab9eaf17923d8d234a6a8b1e1b21a1ee9f2b8
민석 <108412831+ka-MS@users.noreply.github.com>
2023-05-08 22:20:18 +0900

    setting

---
+++ src/main/java/com/jscode/minseok/MinseokApplication.java
@@ -0,0 +1,13 @@
+package com.jscode.minseok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinseokApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinseokApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jscode/minseok/MinseokApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jscode.minseok;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinseokApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
